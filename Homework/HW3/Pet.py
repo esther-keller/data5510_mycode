97fb57405cb89d7fe86290bf86690208bc3d24b2
Esther Keller <estherkeller3@gmail.com>
2026-09-12 18:57:53 -0600

    This is the more efficient version of hw3 part 3

--- Homework/HW3/Pet.py
+++ Homework/HW3/Pet.py
@@ -22,14 +22,16 @@ class Pet:
         self.species = species
 
     def age_in_human_years(self):
-        if self.species.lower() == 'dog':
-            return self.age * 7
+        human_years = { "dog": 7, "cat": 6, "fish": 5 } #chat told me this is more efficient
+        return self.age * human_years.get(self.species.lower(), 1)
+        #didn't delete the old code so I could refference it if I need to
+        '''if self.species.lower() == 'dog': 
         elif self.species.lower() == 'cat':
             return self.age * 6
         elif self.species.lower() == 'fish':
             return self.age * 5
         else:
-            return self.age * 1  # default case for unknown species
+            return self.age * 1  # default case for unknown species'''
     def get_average_lifespan(self):
         return lifespans.get(self.species.lower(), "Unknown species")
 
@@ -38,18 +40,16 @@ dog = Pet("Mushu", 15, "Dog")
 cat = Pet("Little Brother", 3, "Cat")
 fish = Pet("Nemo", 1, "Fish")
 
+#make list of pets to use
+pets = [dog, cat, fish]
+
 #print to check
 print()
 print('-----------Age in Human Years-------------------')
-print(f"{dog.name} is a {dog.species}, is {dog.age} year(s) old, and is {dog.age_in_human_years()} in human years.")
-print()
-print(f"{cat.name} is a {cat.species}, is {cat.age} year(s) old, and is {cat.age_in_human_years()} in human years.")
-print()
-print(f"{fish.name} is a {fish.species}, is {fish.age} year(s) old, and is {fish.age_in_human_years()} in human years.")
+for pet in pets:
+    print(f"{pet.name} is a {pet.species}, is {pet.age} year(s) old, and is {pet.age_in_human_years()} in human years.")
 print()
 print('-----------Average Lifespan-------------------')
-print(f"The average lifespan of a {dog.species} is {dog.get_average_lifespan()} years.")
+for pet in pets:
+    print(f"The average lifespan of a {pet.species} is {pet.get_average_lifespan()} years.")
 print()
-print(f"The average lifespan of a {cat.species} is {cat.get_average_lifespan()} years.")
-print()
-print(f"The average lifespan of a {fish.species} is {fish.get_average_lifespan()} years.")
