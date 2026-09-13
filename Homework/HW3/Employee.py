b4e199392960b2dd1b0a33a51cf20fc4b1e8d619
Esther Keller <estherkeller3@gmail.com>
2026-09-12 18:00:47 -0600

    This is for quetsion 2

---
+++ Homework/HW3/Employee.py
@@ -0,0 +1,22 @@
+'''Create a class called Employee with attributes name and salary. 
+Implement a method within the class that increases the salary of 
+the employee by a given percentage. 
+Instantiate an object of the Employee class with 
+name = "John" and salary = 5000, increase the salary by 10%, 
+and print the updated salary.'''
+
+# Make the Class (the Blueprint)
+class Employee:
+    def __init__(self, name, salary):
+        # save information into the object
+        self.name = name
+        self.salary = salary
+
+    def increase_salary(self, percentage):
+        # increase the salary by a given percentage
+        self.salary += self.salary * (percentage / 100)
+
+# create an object
+employee1 = Employee("John", 5000)
+employee1.increase_salary(10)
+print("Updated salary = $" + str(employee1.salary))
--- Homework/HW3/Rectangle.py
+++ Homework/HW3/Rectangle.py
@@ -1,3 +1,8 @@
+'''Create a class called Rectangle with attributes length and width. 
+Implement a method within the class to calculate the area of the 
+rectangle. Instantiate an object of the Rectangle class 
+with length = 5 and width = 3, and print its area.'''
+
 #Make the Class (the Blueprint)
 class Rectangle:
     def __init__(self, length, width):
@@ -7,8 +12,7 @@ class Rectangle:
     def calculate_area(self):
         # do something with the data
         return self.length * self.width
-#        return "Area = " + str(self.length * self.width) + "units^2"
 # create an object
-Rectangle1 = Rectangle(5,3)
+rectangle1 = Rectangle(5,3)
 # print the result
-print("Area = " + str(Rectangle1.calculate_area()) + " units^2")
+print("Area = " + str(rectangle1.calculate_area()) + " units^2")
