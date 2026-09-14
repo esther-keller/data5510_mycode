a309c63e0fdb3778ba5f9b1b93f28f4fdc112374
Esther Keller <estherkeller3@gmail.com>
2026-09-14 10:40:46 -0600

    Reorganized my notes

--- In_Class_Practice/Sept09.py
+++ In_Class_Practice/Sept09.py
@@ -36,3 +36,31 @@ print(saige_person.favorite_colors)
     # an attribute is a variable inside a class
     # an object is what you build with it
     # innit is a constructor, it runs when you create an object
+
+
+'''--------personal practice---------'''
+#funtion specific to data points
+#create a class with 3 objects
+
+class Dog():
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+
+    def __str__(self):
+        return self.name + " is " + str(self.age) + " years old and their breed is: " + str(self.breed)
+
+    def set_name(self,name):
+        self.name = name
+
+Mushu_Dog = Dog("Mushu", 15, "Poodle Terrior Mix")
+Jane_Dog = Dog("Jane", 5, "Unknown Mix")
+
+print(Mushu_Dog)
+print()
+print(Jane_Dog)
+print()
+print(Mushu_Dog.age)
+print(Mushu_Dog.name)
+print(Mushu_Dog.breed)
